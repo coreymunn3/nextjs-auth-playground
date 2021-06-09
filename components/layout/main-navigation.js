9d8c40e4104d16a50c1b59278f8a037ccb4926d4
Michael Corey Munn <coreymunn3@gmail.com>
2021-06-09 11:24:35 -0400

    Implemented Signout

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,8 +1,15 @@
 import Link from 'next/link';
+import { useSession, signOut } from 'next-auth/client';
 
 import classes from './main-navigation.module.css';
 
 function MainNavigation() {
+  const [session, loading] = useSession();
+
+  const handleSignOut = () => {
+    signOut({ callbackUrl: 'http://localhost:3000/auth' });
+  };
+
   return (
     <header className={classes.header}>
       <Link href='/'>
@@ -12,15 +19,22 @@ function MainNavigation() {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link href='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link href='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!session && !loading && (
+            <li>
+              <Link href='/auth'>Login</Link>
+            </li>
+          )}
+
+          {session && (
+            <li>
+              <Link href='/profile'>Profile</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <button onClick={handleSignOut}>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
