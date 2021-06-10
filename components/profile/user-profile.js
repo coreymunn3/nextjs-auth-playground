9e181036d54c11644c773a8da024e10d6fc72d9d
Michael Corey Munn <coreymunn3@gmail.com>
2021-06-10 11:14:32 -0400

    Protected Profile page by adding serversideprops to profile.js, which calls getSession and redirects if user is not logged in. Also added client side code in user-profile.js to test, though it was not as elegant a solution. Leaving it in as comments.

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,8 +1,29 @@
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
+// *** for client side session auth checking...
+// *** don't need because we're doing serverside in profile
+// import { getSession } from 'next-auth/client';
+// import { useState, useEffect } from 'react';
+// import { useRouter } from 'next/router';
 
 function UserProfile() {
-  // Redirect away if NOT auth
+  // *** for client side session auth checking...
+  // *** don't need because we're doing serverside in profile
+  // const router = useRouter();
+  // const [isLoading, setIsLoading] = useState(true);
+  // useEffect(() => {
+  //   getSession().then((session) => {
+  //     if (!session) {
+  //       router.push('/auth');
+  //     } else {
+  //       setIsLoading(false);
+  //     }
+  //   });
+  // }, []);
+
+  // if (isLoading) {
+  //   return <p style={{ textAlign: 'center' }}>Loading...</p>;
+  // }
 
   return (
     <section className={classes.profile}>
--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,27 @@
 import UserProfile from '../components/profile/user-profile';
+import { getSession } from 'next-auth/client';
 
 function ProfilePage() {
   return <UserProfile />;
 }
 
 export default ProfilePage;
+
+export async function getServerSideProps(context) {
+  const { req } = context;
+  const session = await getSession({ req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+
+  // else, user is logged in
+  return {
+    props: { session },
+  };
+}
